3630dd77ffb99bc9f5860dab18cb7d1f583033be
alexfinch7 <atf325@nyu.edu>
2026-01-05 14:01:34 -0500

    Add privacy policy

---
+++ src/app/privacy/page.tsx
@@ -0,0 +1,19 @@
+import { promises as fs } from 'fs';
+import path from 'path';
+
+export const dynamic = 'force-dynamic';
+
+export default async function PrivacyPage() {
+	const filePath = path.join(process.cwd(), 'src/app/privacy/privacy.html');
+	const htmlContent = await fs.readFile(filePath, 'utf8');
+
+	return (
+		<div className="w-full min-h-screen bg-white text-black">
+			<div 
+				className="max-w-4xl mx-auto p-6 md:p-12"
+				dangerouslySetInnerHTML={{ __html: htmlContent }} 
+			/>
+		</div>
+	);
+}
+
